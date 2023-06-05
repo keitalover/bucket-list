a5de26df3eefc08e7a7721bca3ac3cf6ef365241
jihantriananda <jihantri727@gmail.com>
2023-06-05 21:09:39 +0700

    test

--- app.py
+++ app.py
@@ -1,37 +1,62 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://???:sparta@cluster0.gaybq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+client = MongoClient("mongodb://test:test@ac-oowgeoe-shard-00-00.twt800w.mongodb.net:27017,ac-oowgeoe-shard-00-01.twt800w.monmgodb.net:27017,ac-oowgeoe-shard-00-02.twt800w.mongodb.net:27017/?ssl=true&replicaSet=atlas-5kccjl-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.dbsparta
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
-    bucket_receive = request.form["bucket_give"]
+    # sample_receive = request.form['sample_give']
+    bucket_receive = request.form['bucket_give']
+
     count = db.bucket.count_documents({})
     num = count + 1
     doc = {
-        'num':num,
+        'num': num,
         'bucket': bucket_receive,
-        'done':0
+        'done': 0
     }
     db.bucket.insert_one(doc)
     return jsonify({'msg': 'data saved!'})
 
 @app.route("/bucket/done", methods=["POST"])
 def bucket_done():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': 'POST /bucket/done request!'})
+    num_receiver = request.form['num_give']
+    db.bucket.update_one(
+        {'num': int(num_receiver) },
+        {'$set': {'done': 1 }}
+    )
+    return jsonify({'msg': 'update done!'})
+
+@app.route("/delete", methods=["POST"])
+def delete_bucket():
+    num_receiver = request.form['num_give']
+    db.bucket.delete_one({'num': int(num_receiver)})
+    return jsonify({'msg': 'delete done!'})
+
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
-    return jsonify({'msg': 'GET request!'})
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    return jsonify({'buckets': bucket_list})
+
+
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
